91bf8beb7e40ec0f26613d50cc4f71f96a371070
Ishani Srivastava <ishasri.1212@gmail.com>
2021-01-23 16:06:38 +0530

    Change file path for images folder

--- choose_wallpaper.sh
+++ choose_wallpaper.sh
@@ -5,7 +5,7 @@ PID=$(pgrep gnome-session| tail -n1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 # This chooses an image randomly from the folder. 
-DIR="/home/ishani/astroPicsScraper/astroPicsScraper/images"
+DIR="/path/to/spaceWallpapers/images"
 PIC=$(ls $DIR/* | shuf -n1)
 echo "pic chosen $PIC"
 gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
